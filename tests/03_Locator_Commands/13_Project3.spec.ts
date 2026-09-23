f11729360369da5803f535c2ba4c769041694e3f
Rahul5495 <rahulmangate1141@gmail.com>
2026-09-23 20:40:31 +0530

    added session-storage and allure reports

---
+++ tests/03_Locator_Commands/13_Project3.spec.ts
@@ -0,0 +1,19 @@
+
+import{test, expect} from "@playwright/test";
+
+test("Verfiy the error message in the wingify free trial", async({page})=>{
+
+       await page.goto("https://wingify.com/free-trial/");
+       let inputBox= page.locator("//input[@id='free-trial-step1-email']");
+       await inputBox.fill("abcdefg");
+       await page.locator("#free-trial-step1-gdpr-consent-checkboxcu-marketing-consent-checkbox").click();
+       await page.locator("[data-qa='free-trial-step1-gdpr-consent-checkboxgdpr-consent-checkbox']").click();
+       await page.locator("//button[@data-qa='page-su-submit']").first().click();
+       let error_message= page.locator("//div[contains(@class,'invalid-reason')]").first();
+
+       let error_message_text= await error_message.textContent();
+       expect(error_message_text).toContain("The email address you entered is incorrect.");
+
+       await page.pause();
+        
+});
---
+++ tests/04_Session_Storage/17_TestWingify.spec.ts
@@ -0,0 +1,23 @@
+
+import{test, expect} from "@playwright/test";
+
+// Load the saved session
+
+test.use({
+    storageState: './user-session.json'
+});
+
+test("go directly to dashboard — Test1", async({page})=>{
+   await page.goto("https://app.wingify.com/#/dashboard?accountId=1281316");
+   await expect(page).toHaveURL(/dashboard/);
+   console.log("Dashboard loaded — no login needed ✅");
+   await page.waitForTimeout(3000);
+
+});
+
+test("go directly to dashboard2 — Test2", async({page})=>{
+   await page.goto("https://app.wingify.com/#/dashboard?accountId=1281316");
+   await expect(page).toHaveURL(/dashboard/);
+   console.log("Dashboard2 loaded — no login needed ✅");
+   await page.waitForTimeout(3000);
+});
